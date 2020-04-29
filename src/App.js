48389323b9ce0a07de48343d7ee1c94500c7ce67
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 12:59:49 +0530

    added route for each dog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import whiskey from "./images/whiskey.jpg";
 import hazel from "./images/hazel.jpg";
 import tubby from "./images/tubby.jpg";
 import DogList from "./DogList";
+import DogDetails from "./DogDetails";
 
 class App extends Component {
   static defaultProps = {
@@ -42,8 +43,22 @@ class App extends Component {
     ],
   };
   render() {
+    const getDog = (props) => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        (dog) => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
     return (
-      <Route path="/dogs" render={() => <DogList dogs={this.props.dogs } />} />
+      <Switch>
+        <Route
+          exact
+          path="/dogs"
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path="/dogs/:name" render={getDog} />
+      </Switch>
     );
   }
 }
