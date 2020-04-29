d4756e3fcdf39e6b065f7baa5b97ec17c3dbc7da
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 18:03:05 +0530

    Updates

---
+++ precache-manifest.28c2061aef98ce18b533c5ad1edf2ba3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd646d6b162ddfcfff72d7faf560a8af",
+    "url": "/Doggo-Finder/index.html"
+  },
+  {
+    "revision": "e7f0787ad43637620e09",
+    "url": "/Doggo-Finder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "eb852c3565424b4d6478",
+    "url": "/Doggo-Finder/static/css/main.24d8d738.chunk.css"
+  },
+  {
+    "revision": "e7f0787ad43637620e09",
+    "url": "/Doggo-Finder/static/js/2.3dee9b19.chunk.js"
+  },
+  {
+    "revision": "150e7ebf1b16402b62fb665d1a7869df",
+    "url": "/Doggo-Finder/static/js/2.3dee9b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb852c3565424b4d6478",
+    "url": "/Doggo-Finder/static/js/main.0c2cfd28.chunk.js"
+  },
+  {
+    "revision": "d8a2ce111724669fae36",
+    "url": "/Doggo-Finder/static/js/runtime-main.913cc56f.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/Doggo-Finder/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/Doggo-Finder/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/Doggo-Finder/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
