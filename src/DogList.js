0c755031fa07577b1f15b662cbab8512c5f47561
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 17:55:51 +0530

    added margin in y direction

--- src/DogList.js
+++ src/DogList.js
@@ -6,7 +6,7 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-2 text-center">Dog List!</h1>
+        <h1 className="display-2 text-center my-4">Dog List!</h1>
         <div className="row">
           {this.props.dogs.map((d) => (
             <div className="Dog col-lg-4 text-center" key={d.name}>
