936f4cb0698a4ab270adc145c2b86b90b7567e82
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 11:29:22 +0530

    converted app to a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div>HI</div>;
+  }
 }
 
 export default App;
