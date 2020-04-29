243515d109ab3bedddf6ea871a075bebf9b0aa6a
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 13:15:27 +0530

    added dog detail component

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+// import "./DogDetails.css";
 
 class DogDetails extends Component {
   render() {
+    let { dog } = this.props;
     return (
-      <div>
-        <h1>{this.props.dog.name}</h1>
+      <div className="container">
+        <div className="DogDetails row">
+          <div className="col-11 col-lg-5">
+            <div className="DogDetails-card card">
+              <img className="card-img-top" src={dog.src} alt={dog.name} />
+              <div className="card-body">
+                <h2 className="card-title">{dog.name}</h2>
+                <h4 className="card-subtitle text-muted">
+                  {dog.age} years old
+                </h4>
+              </div>
+              <ul className="list-group list-group-flush">
+                {dog.facts.map((fact, i) => (
+                  <l1 className="list-group-item" key={i}>
+                    {fact}
+                  </l1>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
