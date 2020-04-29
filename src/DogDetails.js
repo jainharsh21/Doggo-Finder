da5216846620b6e0f557179671905d6089ad39a3
HARSH JAIN <jainharsh270@gmail.com>
2020-04-29 17:35:51 +0530

    refactored the code(container)

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -6,30 +6,26 @@ class DogDetails extends Component {
   render() {
     let { dog } = this.props;
     return (
-      <div className="container">
-        <div className="DogDetails row justify-content-center mt-5">
-          <div className="col-11 col-lg-5">
-            <div className="DogDetails-card card">
-              <img className="card-img-top" src={dog.src} alt={dog.name} />
-              <div className="card-body">
-                <h2 className="card-title">{dog.name}</h2>
-                <h4 className="card-subtitle text-muted">
-                  {dog.age} years old
-                </h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                {dog.facts.map((fact, i) => (
-                  <l1 className="list-group-item" key={i}>
-                    {fact}
-                  </l1>
-                ))}
-              </ul>
-              <div className="card-body">
-                <Link to="/dogs" className="btn btn-info">
-                  {" "}
-                  Go Back!{" "}
-                </Link>
-              </div>
+      <div className="DogDetails row justify-content-center mt-5">
+        <div className="col-11 col-lg-5">
+          <div className="DogDetails-card card">
+            <img className="card-img-top" src={dog.src} alt={dog.name} />
+            <div className="card-body">
+              <h2 className="card-title">{dog.name}</h2>
+              <h4 className="card-subtitle text-muted">{dog.age} years old</h4>
+            </div>
+            <ul className="list-group list-group-flush">
+              {dog.facts.map((fact, i) => (
+                <l1 className="list-group-item" key={i}>
+                  {fact}
+                </l1>
+              ))}
+            </ul>
+            <div className="card-body">
+              <Link to="/dogs" className="btn btn-info">
+                {" "}
+                Go Back!{" "}
+              </Link>
             </div>
           </div>
         </div>
